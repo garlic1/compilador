8ad1408387fbf506f3cd767c8394b22c24e3840a
gabriel <gabriel.chimifosk@gmail.com>
2023-07-30 01:14:20 -0300

    etapa4 files

---
+++ etapa4/main.c
@@ -0,0 +1,35 @@
+/*
+Função principal para realização da análise sintática.
+
+Este arquivo será posterioremente substituído, não acrescente nada.
+*/
+#include <stdio.h>
+#include "ast.h"
+#include "parser.tab.h" //arquivo gerado com bison -d parser.y
+
+extern int yylex_destroy(void);
+
+void *arvore = NULL;
+
+void exporta(asd_tree *tree)
+{
+  if (tree == NULL){
+    return;
+  }else{
+    printf("%p [label = \"%s\"]\n", tree, tree->token.valor_token);
+    //printf("%p [label = \"%s\", %d, %d]\n", tree, tree->token.valor_token, tree->token.line_number, tree->token.tipo);
+    for(int i = 0; i < tree->number_of_children; i++)
+    {
+        printf("%p, %p\n", tree, tree->children[i]);        
+        exporta(tree->children[i]);
+    }    
+  }
+}
+
+int main (int argc, char **argv)
+{
+  int ret = yyparse();
+  exporta (arvore);
+  yylex_destroy();
+  return ret;
+}
